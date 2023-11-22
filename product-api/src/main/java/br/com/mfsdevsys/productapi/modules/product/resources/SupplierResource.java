43114b07fb9d10710f03e5093d820608a87ceeb7
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2023-11-22 11:06:36 -0400

    Update Supplier

--- product-api/src/main/java/br/com/mfsdevsys/productapi/modules/product/resources/SupplierResource.java
+++ product-api/src/main/java/br/com/mfsdevsys/productapi/modules/product/resources/SupplierResource.java
@@ -8,11 +8,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import br.com.mfsdevsys.productapi.modules.product.dto.CategoryDTO;
 import br.com.mfsdevsys.productapi.modules.product.dto.SupplierDTO;
 import br.com.mfsdevsys.productapi.modules.product.service.SupplierService;
 
@@ -64,10 +66,16 @@ public class SupplierResource {
 	public ResponseEntity<SupplierDTO> insert(@RequestBody SupplierDTO dto){
 		dto = service.insert(dto) ;
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-				       .buildAndExpand( dto.getId() ).toUri();
-		
+				       .buildAndExpand( dto.getId() ).toUri();	
 		return ResponseEntity.created( uri ).body( dto );
 		
 	}
+	
+	@PutMapping(value= "/{id}")
+	public ResponseEntity<SupplierDTO> update(@PathVariable Integer id, @RequestBody SupplierDTO dto){
+		dto = service.update(id, dto) ;
+		return ResponseEntity.ok().body( dto );
+	}
+	
 
 }
